95feb3466a8b2341911fe095c26cc7e96985c320
Leon <lv@oxon.ch>
2025-04-19 19:13:27 +0200

    Add some timeout to the Turbo warnings, to prevent false positive

--- src/lib/DiagnosticsChecker.js
+++ src/lib/DiagnosticsChecker.js
@@ -18,9 +18,18 @@ export default class DiagnosticsChecker {
 
   #checkForTurboDrive = () => {
     if (!window.Turbo) {
-      this.printWarning("Turbo is not detected. Hotwire Native will not work correctly without Turbo")
+      // Since it's possible that the DevTools are loaded before Turbo, we need to wait a bit to check if Turbo is loaded
+      setTimeout(() => {
+        if (!window.Turbo) {
+          this.printWarning("Turbo is not detected. Hotwire Native will not work correctly without Turbo")
+        }
+      }, 1000)
     } else if (window.Turbo?.session.drive === false) {
-      this.printWarning("Turbo Drive is disabled. Hotwire Native will not work correctly without Turbo Drive")
+      setTimeout(() => {
+        if (window.Turbo?.session.drive === false) {
+          this.printWarning("Turbo Drive is disabled. Hotwire Native will not work correctly without Turbo Drive")
+        }
+      }, 1000)
     }
   }
 
